d0b22fd8e061c75a4fd8fc73d76c497d50be9d00
dnguye234 <dnguye2@uw.edu>
2023-02-20 23:13:25 -0800

    update values

--- Summary.R
+++ Summary.R
@@ -1,6 +1,17 @@
-summary_values <- list(not_paid_course, paid_course)
+library(dplyr)
+student_alc_consumption <- read.csv("student-por.csv")
+student_alc_consumption <- student_alc_consumption %>%
+  mutate(count = 1)
 
+num_walc <- student_alc_consumption %>%
+  group_by(Walc == 3) %>%
+  summarise(sum(count)) %>%
+  pull()
 
+rating <- student_alc_consumption %>%
+  group_by(famrel == 3) %>%
+  summarise(avg_walc = mean(as.numeric(Walc))) %>%
+  pull()
 
 
 not_paid_course <- student_alc_consumption %>%
@@ -16,3 +27,5 @@ paid_course <- student_alc_consumption %>%
   filter(Dalc >= 2) %>% 
   count() %>% 
   pull()
+
+summary_values <- list(not_paid_course, paid_course, num_walc, rating)
