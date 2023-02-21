27ea5de95cdda41cd08af4a0178025d2927423d2
nishant120 <nhs7@uw.edu>
2023-02-20 23:38:09 -0800

    Updated

--- Table.R
+++ Table.R
@@ -1,7 +1,13 @@
 library(dplyr)
-setwd("~/Downloads")
-student_alc <- read.csv("student-por.csv")
+setwd("~/Desktop")
+getwd()
+student_alc <- read.csv("/Users/nish1209/Desktop/student-por.csv", FALSE)
 View(student_alc)
+
+student_alc <- student_alc%>% 
+  select(V2, V3, V5, V6, V9, V10, V11, V12, V15, V17, V18, V19, V21, V22, V24, V25, V26, V27, V28, V29, V30, V33)
+names(student_alc) <- student_alc[1,]
+student_alc <- student_alc[-1,]
 
 table_student_alc <- select(student_alc, sex, age, 'Parental Status' = Pstatus , "Family Relationship" = famrel , "Going Out With Friends" = goout, "Weekday Alc Consumption" = Dalc, "Weekend Alc Consumption" = Walc, "Health Condition" = health  )
 
@@ -10,3 +16,4 @@ table_student_alc <- table_student_alc %>% filter(age < 18 ) %>% head(50)
 table_student_alc <- table_student_alc %>%  group_by(sex, `Parental Status`) %>% arrange(sex, `Parental Status`)
 
 View(table_student_alc)
+
