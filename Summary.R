9d9ecb345254c6db040e7bad61693972b2ed7ecc
MorVered7 <mvered@uw.edu>
2023-02-20 21:50:15 -0800

    Added chart 1 summary values

--- Summary.R
+++ Summary.R
@@ -1,1 +1,18 @@
+summary_values <- c(not_paid_course, paid_course)
 
+
+
+
+not_paid_course <- student_alc_consumption %>%
+  select(Dalc, paid) %>% 
+  filter(paid == "no") %>%
+  filter(Dalc >= 2) %>% 
+  count() %>% 
+  pull()
+
+paid_course <- student_alc_consumption %>%
+  select(Dalc, paid) %>% 
+  filter(paid == "yes") %>%
+  filter(Dalc >= 2) %>% 
+  count() %>% 
+  pull()
